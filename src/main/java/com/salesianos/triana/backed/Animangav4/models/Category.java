8ab3be38a8a2f5e83b131977a03784dc179dec00
Arturo Céspedes Pedrazas <cespedes.peart22@triana.salesianos.edu>
2023-02-12 23:29:02 +0100

    01-Commit inicial, entidades creadas

---
+++ src/main/java/com/salesianos/triana/backed/Animangav4/models/Category.java
@@ -0,0 +1,41 @@
+package com.salesianos.triana.backed.Animangav4.models;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator",
+            parameters = {
+                    @Parameter(
+                            name = "uuid_get_strategy_class",
+                            value = "org.hibernate.id.uuid.CustomVersionStrategy"
+                    )
+            }
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    private String name;
+
+
+    private String description;
+
+}
---
+++ src/main/java/com/salesianos/triana/backed/Animangav4/models/Manga.java
@@ -0,0 +1,53 @@
+package com.salesianos.triana.backed.Animangav4.models;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Manga implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator",
+            parameters = {
+                    @Parameter(
+                            name = "uuid_gen_strategy_class",
+                            value = "org.hibernate.id.uuid.CustomVersionOneStrategy"
+
+                    )
+            }
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private String posterPath;
+
+    private Category category;
+
+    private String publisher;
+
+
+
+
+}
